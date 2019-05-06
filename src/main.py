5096ae0e691f93fed8dd4555f10544758656f11a
Charlotte <le.du.charlotte@gmail.com>
2019-05-06 22:32:32 +0200

    get auth

--- src/main.py
+++ src/main.py
@@ -1,16 +1,25 @@
 from flask import Flask
-from flask_restplus import Resource, Api
+from flask_restplus import Resource, Api, reqparse
 from flask_cors import CORS
 
 app = Flask(__name__)
-api = Api(app)
+app.config['BUNDLE_ERRORS'] = True
+api = Api(app, version="1.0", title="Back-end")
 CORS(app)
 
 
-@api.route('/hello')
-class HelloWorld(Resource):
+data = reqparse.RequestParser()
+data.add_argument('email', type=str, required=True, location='args')
+data.add_argument('psw', type=str, required=True, location='args')
+
+
+@api.route('/signIn',  endpoint='with-parser')
+class SignIn(Resource):
+    @api.expect(data)
     def get(self):
-        return {'hello': 'world'}
+        args = data.parse_args(strict=True)
+        print(args['email'] + " " + args['psw'])
+        return {'token': 'token'}
 
 
 if __name__ == '__main__':
