0359f0b1db1b7088b2b69625a06e9322b716a44d
LarcherJulien <julienlarcher971@gmail.com>
2019-06-24 23:07:22 +0200

    commit api

--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
 import io
-
 import cv2
 import numpy as np
 from PIL import Image
@@ -9,6 +8,7 @@ from flask_cors import CORS
 from flask_restplus import Resource, Api, reqparse
 from flask_restplus import abort
 from werkzeug.datastructures import FileStorage
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 app.config['BUNDLE_ERRORS'] = True
@@ -55,6 +55,24 @@ class MyFileUpload(Resource):
             abort(400, 'error when get the image file')
         return {'status': 'Done'}
 
+
+@app.route('/form')
+def form():
+    return render_template('form.html')
+
+@app.route('/submitted', methods=['POST'])
+def submitted_form():
+    name = request.form['name']
+    email = request.form['email']
+    site = request.form['site_url']
+    comments = request.form['comments']
+
+    return render_template(
+        'submitted_form.html',
+        name=name,
+        email=email,
+        site=site,
+        comments=comments)
 
 if __name__ == '__main__':
     app.run(debug=True)
