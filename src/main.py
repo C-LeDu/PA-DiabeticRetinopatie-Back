6990ba21db5f986617fe9b71cdf22cdba17e99d9
Charlotte <le.du.charlotte@gmail.com>
2019-06-25 20:51:03 +0200

    add lib

--- src/main.py
+++ src/main.py
@@ -70,23 +70,5 @@ class MyFileUpload(Resource):
         return {'status': 'Done'}
 
 
-# @app.route('/form')
-# def form():
-#     return render_template('form.html')
-#
-# @app.route('/submitted', methods=['POST'])
-# def submitted_form():
-#     name = request.form['name']
-#     email = request.form['email']
-#     site = request.form['site_url']
-#     comments = request.form['comments']
-#
-#     return render_template(
-#         'submitted_form.html',
-#         name=name,
-#         email=email,
-#         site=site,
-#         comments=comments)
-
-#if __name__ == '__main__':
-#    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
