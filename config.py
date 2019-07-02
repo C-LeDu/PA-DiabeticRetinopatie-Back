aa6d27b3819adefd777a6178d459d2c8bcaf1c72
Charlotte <le.du.charlotte@gmail.com>
2019-07-02 12:10:53 +0200

    change config workers

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-import multiprocessing
 from os import environ as env
 
 PORT = int(env.get("PORT", 8080))
@@ -6,5 +5,5 @@ DEBUG_MODE = int(env.get("DEBUG_MODE", 1))
 
 # Gunicorn config
 bind = ":" + str(PORT)
-workers = 2 * multiprocessing.cpu_count() + 1
+workers = 1
 threads = workers
